29659934d0fa1a5d8a3489eea390089b2bd63038
Jarek Rettinghouse <j.rettinghouse@gmail.com>
2024-01-13 16:11:41 -0800

    User input required for structure of organized media into month/, year/, year_month/, and year/month/.

--- FileHandler.py
+++ FileHandler.py
@@ -6,23 +6,23 @@ import ffmpeg
 from tqdm import tqdm
 from PIL import Image
 
-DEFAULT_DEST = "MetadataNotFound"
+DEFAULT_DEST = 'Timestamp_Unavailable'
 
 
 def find_media_in_dir(files):
     '''Return all jpg files in directory'''
     photo_media = []
     video_media = []
-    photo_types = ["jpg", "png"]
-    video_types = ["mp4"]
+    photo_types = ['jpg', 'png']
+    video_types = ['mp4']
 
     for file_name in files:
         # split and check last token, -> jpg
-        tokens = file_name.split(".")
+        tokens = file_name.split('.')
         if tokens[-1] in photo_types:
-            photo_media.append(f"{file_name}")
+            photo_media.append(f'{file_name}')
         elif tokens[-1] in video_types:
-            video_media.append(f"{file_name}")
+            video_media.append(f'{file_name}')
 
     return photo_media, video_media
 
@@ -35,8 +35,8 @@ def create_dir(filepath):
 
 def move_media(file_dir, media_file, dest):
     '''Move media file to destination'''
-    original = f"{file_dir}{media_file}"
-    dest = f"{dest}/{media_file}"
+    original = f'{file_dir}{media_file}'
+    dest = f'{dest}/{media_file}'
     shutil.move(original, dest)
 
 
@@ -50,13 +50,32 @@ def add_record(transactions, media_file, dest):
 def pp_transactions(transactions):
     '''Pretty Print Transactions records'''
     for k, v in sorted(transactions.items()):
-        msg = f"\n{k}:"
+        msg = f'\n{k}:'
         for val in v:
-            msg += f"\n    {val}"
+            msg += f'\n    {val}'
         print(msg)
 
 
-def video_copy_n_sort(videos, source_dir, transactions):
+def generate_date_dir(source_dir, year, month, dir_layout):
+    '''Generate correct requested date dir based on user input'''
+    date_dir = ""
+
+    if dir_layout == 'month':
+        date_dir = f'{source_dir}{month}'
+    elif dir_layout == 'year':
+        date_dir = f'{source_dir}{year}'
+    elif dir_layout == 'year_month':
+        date_dir = f'{source_dir}{year}_{month}'
+    elif dir_layout == 'year/month':
+        date_dir = f'{source_dir}{year}/{month}'
+    else:
+        print('Invalid dir_layout provided')
+        sys.exit(1)
+
+    return date_dir
+
+
+def video_copy_n_sort(videos, source_dir, transactions, dir_layout):
     '''Logic handling copying and organizing video files'''
     default_dir = f'{source_dir}{DEFAULT_DEST}'
 
@@ -78,9 +97,9 @@ def video_copy_n_sort(videos, source_dir, transactions):
             sys.exit(1)
 
         try:
-            tokens = video_stream['tags']['creation_time'].split("-")
-            date_dir = f'{source_dir}{tokens[0]}_{tokens[1]}'
-        except Exception:
+            tokens = video_stream['tags']['creation_time'].split('-')
+            date_dir = generate_date_dir(source_dir, tokens[0], tokens[1], dir_layout)
+        except IndexError:
             date_dir = default_dir
 
         create_dir(date_dir)
@@ -88,7 +107,7 @@ def video_copy_n_sort(videos, source_dir, transactions):
         add_record(transactions, vid, date_dir)
 
 
-def img_copy_n_sort(jpg_files, source_dir, transactions):
+def img_copy_n_sort(jpg_files, source_dir, transactions, dir_layout):
     '''Logic handling copying and organizing photos'''
     # Create default directory
     default_dir = f'{source_dir}{DEFAULT_DEST}'
@@ -97,8 +116,8 @@ def img_copy_n_sort(jpg_files, source_dir, transactions):
     for pic in tqdm(jpg_files):
         try:
             img_file = Image.open(f'{source_dir}{pic}')
-        except Exception as e:
-            print(f"Invalid Image: {e}")
+        except IOError as e:
+            print(f'Invalid Image: {e}')
 
         exif = img_file.getexif()
         timestamp = exif.get(306, None)
@@ -107,10 +126,10 @@ def img_copy_n_sort(jpg_files, source_dir, transactions):
         # Timestamp Metadata found, prepare date directory
         if timestamp is not None:
             time_tokens = timestamp.split()
-            date = time_tokens[0].split(":")
-            date_dir = f"{source_dir}{date[0]}_{date[1]}"
+            date = time_tokens[0].split(':')
+            date_dir = generate_date_dir(source_dir, date[0], date[1], dir_layout)
 
-        create_dir(default_dir)
+        create_dir(date_dir)
         move_media(source_dir, pic, date_dir)
         add_record(transactions, pic, date_dir)
 
@@ -127,26 +146,42 @@ def main(source_dir):
 
     # Return message if no media is found
     if len(imgs) == 0 and len(videos) == 0:
-        print("No Media found in !")
-        return
+        print(f'No Media found in {os.getcwd()}/{source_dir}!')
+        sys.exit(1)
 
-    msg = f"{len(imgs)} images and {len(videos)} videos found in {os.getcwd()}/{source_dir}"
+    msg = f'{len(imgs)} images and {len(videos)} videos found in {os.getcwd()}/{source_dir}'
     print(msg)
 
     valid_res = False
     # ask for confirmation until expected reponse or exit
     while valid_res is False:
-        res = input("\nWould you like to sort these images? (y/n): ")
-        if res.strip() == "y":
+        res = input('\nWould you like to sort these images? (y/n): ')
+        if res.strip() == 'y':
             valid_res = True
-        elif res.strip() == "n":
-            print("Abort recieved, no images moved.")
-            return
+        elif res.strip() == 'n':
+            print('Abort recieved, no images moved.')
+            sys.exit(1)
 
-    # Copy images into year_month dir and display transactions to user
+    dir_layout = None
+    # ask for confirmation regarding dir layout
+    while dir_layout is None:
+        res = input(
+            '''\nWhich directory layout would you prefer?:
+            year (y), month (m), year_month? (y_m), or year/month (y/m): ''')
+        if res.strip() == 'y':
+            dir_layout = 'year'
+        elif res.strip() == 'm':
+            dir_layout = 'month'
+        elif res.strip() == 'y_m':
+            dir_layout = 'year_month'
+        elif res.strip() == 'y/m':
+            dir_layout = 'year/month'
+        else:
+            print('Invalid input, please try again.')
+
     transactions = {}
-    img_copy_n_sort(imgs, source_dir, transactions)
-    video_copy_n_sort(videos, source_dir, transactions)
+    img_copy_n_sort(imgs, source_dir, transactions, dir_layout)
+    video_copy_n_sort(videos, source_dir, transactions, dir_layout)
 
     pp_transactions(transactions)
 
@@ -157,7 +192,7 @@ if __name__ == "__main__":
         # Format: ../ or DIR/
         SOURCE_DIR = str(sys.argv[1])
     except IndexError:
-        print("Optional path not given, current directory used as default.")
-        SOURCE_DIR = "/"
+        print('Optional path not given, current directory used as default.')
+        SOURCE_DIR = '/'
 
     main(SOURCE_DIR)
